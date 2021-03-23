6c675c56014e21b187b6023f9260354240b6b059
AlanaNabihahT <81225981+AlanaNabihahT@users.noreply.github.com>
2021-03-23 16:30:37 +0700

    Update JavaLibs5026201109.java

--- JavaLibs5026201109.java
+++ JavaLibs5026201109.java
@@ -1,21 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javalibs5026201109;
 
 import java.awt.*;
 import javax.swing.JOptionPane;
-/**
- *
- * @author Alana
- */
+
 public class JavaLibs5026201109 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showMessageDialog(null,"Welcome!");
